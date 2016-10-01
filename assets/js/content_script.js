3590e2f6a756a88da5ea7f72f1a52a470e0b7b05
collnwalkr <collinwalkerdesigns@gmail.com>
2016-09-30 18:40:00 -0700

    ascii emojis will now be correctly replaced if they are surrounded by whitespace.

--- assets/js/content_script.js
+++ assets/js/content_script.js
@@ -1,5 +1,4 @@
 var allowed_emoticons = [];
-
 // reference : http://emojipedia.org/apple/
 allowed_emoticons = {
 	":)"		: "🙂",
@@ -31,21 +30,60 @@ function setEndOfContenteditable(contentEditableElement){
 	selection.addRange(range);//make the range you have just created the visible selection
 }
 
-document.addEventListener('keydown', function (e) { 
-	var input_div = document.activeElement;
-	if (input_div.tagName == "DIV" && input_div.className == "input")
-		replaceEmoticons(input_div)
+document.addEventListener('keydown', function (e) {
+	//If the space key is pressed
+	if (e.keyCode == 32 || e.keyCode == 13) {
+		var input_div = document.activeElement;
+		if (input_div.tagName == "DIV" && input_div.className == "input")
+			replaceEmoticons(input_div)
+	}
 });
 
 function replaceEmoticons(container){
 	var flag = false;
-	for (var icon in allowed_emoticons)
-		if (container.innerHTML.indexOf(icon)>-1){
-			flag = true;
-			container.innerHTML = container.innerHTML.replace(icon,allowed_emoticons[icon]);
-		}
+
+	for (var icon in allowed_emoticons){
+
+		var current_message = container.innerHTML;
+
+		// Get all occurrences of an icon
+		var all_occurrences = locations(icon, current_message);
+
+		// For each occurrence
+		for(var i = 0; i < all_occurrences.length; i++){
+			var emoji_index = all_occurrences[i];
+
+			// If the ascii emoji is present in the string
+			if (emoji_index>-1){
+
+				// Get the character before and after
+				var char_before = current_message.charAt(emoji_index - 1);
+				var char_after  = current_message.charAt(emoji_index + icon.length);
+
+				// Check if those characters are spaces
+				var is_char_before_valid = (char_before == ' ' || char_before =='' || char_before == '\n');
+				var is_char_after_valid  = (char_after  == ' ' || char_after == '' || char_before == '\n');
+
+				// If the ascii emoji is surrounded by whitespace, replace with the correct character
+				if(is_char_before_valid && is_char_after_valid){
+					flag = true;
+					container.innerHTML = replace(icon, emoji_index, current_message);
+				}
+			} //end if
+		} //end for all_occurrences
+	} //end for icon in allowed_emoticons
 
 	if(flag)
 		setEndOfContenteditable(container);
 	return flag;
+}
+
+function locations(substring,string){
+	var a=[],i=-1;
+	while((i=string.indexOf(substring,i+1)) >= 0) a.push(i);
+	return a;
+}
+
+function replace(icon, index, message){
+	return message.substr(0,index) + allowed_emoticons[icon] + message.substr(index + icon.length, message.length);
 }
